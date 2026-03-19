6e8eede4a85c5b6fa96f88947c3bee7ec728c455
yonn_drakkonuer <emersongoncalves.grangeiro@gmail.com>
2026-03-18 21:45:31 -0300

    Commit Contendo as configuração dos micronaut

--- backend/src/main/java/com/conecta/BackendController.java
+++ backend/src/main/java/com/conecta/BackendController.java
@@ -1,17 +1,13 @@
 package com.conecta;
 
-import io.micronaut.http.MediaType;
-import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
-import io.micronaut.http.annotation.Get;
-import io.micronaut.http.annotation.Post;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.rules.SecurityRule;
 
 @Secured(SecurityRule.IS_ANONYMOUS)
 @Controller("/")
 public class BackendController {
-
+    /*
     @Get(uri = "/", produces = "text/plain")
     public String index() {
         return "Example Response";
@@ -20,5 +16,7 @@ public class BackendController {
     @Post(uri = "/teste", consumes = MediaType.TEXT_PLAIN, produces = MediaType.TEXT_PLAIN)
     public String resp(@Body String resq) {
         return "Resposta: " + resq;
-    }
+    }*/
+    //@Post(uri="/validarCNPJ", consumes = MediaType.TEXT_PLAIN, produces = MediaType.TEXT_PLAIN)
+    //public Boolean Validar(@Body)
 }
